8fd0a408ca20ba641a4401343db56a1b126f77b9
BaoulyNelson <elconquistadorbaoulyn@gmail.com>
2024-04-25 18:54:31 -0400

    chagement3

---
+++ SCRIPT/dateFooter.js
@@ -0,0 +1,5 @@
+// Récupérer l'année actuelle
+const currentYear = new Date().getFullYear();
+
+// Sélectionner l'élément avec l'ID "currentYear" et mettre à jour son contenu textuel avec l'année actuelle
+document.getElementById("currentYear").textContent = currentYear;
--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,153 +1,112 @@
-// JavaScript pour le menu hamburger
-const menuIcon = document.querySelector('.menu-icon');
-const sidebar = document.getElementById('sidebar');
-const searchIcon = document.querySelector('.search-icon');
-const searchForm = document.querySelector('.search-form');
-const searchInput = document.querySelector('.search-form input');
-const closeIcon = document.querySelector('.close-icon');
 
-
-// Gestionnaire d'événements pour le menu hamburger
-menuIcon.addEventListener('click', () => {
-  sidebar.classList.toggle('show');
-});
-
-
-// Gestionnaire d'événements pour l'icône de recherche
-searchIcon.addEventListener('click', () => {
-  if (searchForm.style.display === 'block') {
-    searchForm.style.display = 'none';
-  } else {
-    searchForm.style.display = 'block';
-    searchInput.focus();
-  }
-});
-
-// Gestionnaire d'événements pour l'icône de fermeture
-closeIcon.addEventListener('click', () => {
-  sidebar.classList.remove('show');
-});
-
-
-// JavaScript pour le filtrage en temps réel et la croix pour vider la recherche
-searchInput.addEventListener('input', () => {
-  const filter = searchInput.value.trim().toUpperCase(); // Supprime les espaces avant et après la saisie et convertit en majuscules
-  const links = document.querySelectorAll('.sidebar ul li');
-  let found = 0; // Initialise le compteur de résultats trouvés à 0
+// Attendre que le DOM soit entièrement chargé avant d'exécuter le script
+document.addEventListener('DOMContentLoaded', function() {
   
-  links.forEach(link => {
-      if (link.innerText.toUpperCase().indexOf(filter) > -1) {
-          link.style.display = '';
-          found++; // Incrémente le compteur de résultats trouvés
-      } else {
-          link.style.display = 'none';
-      }
+    // Sélectionner tous les éléments avec la classe "parent"
+    const parents = document.querySelectorAll('.parent');
+    
+    // Pour chaque élément parent sélectionné
+    parents.forEach(parent => {
+      
+      // Sélectionner l'icône de basculement à l'intérieur de l'élément parent
+      const toggleIcon = parent.querySelector('.toggle-icon');
+      
+      // Ajouter un écouteur d'événements pour le clic sur l'élément parent
+      parent.addEventListener('click', function(event) {
+        
+        // Sélectionner le sous-menu à l'intérieur de l'élément parent
+        const subMenu = this.querySelector('.sub-menu');
+        
+        // Vérifier si le sous-menu est caché ou affiché
+        if (subMenu.style.display === 'none' || subMenu.style.display === '') {
+          
+          // Si le sous-menu est caché ou non défini, l'afficher
+          subMenu.style.display = 'block';
+          
+          // Changer l'icône pour indiquer que les sous-liens sont affichés
+          toggleIcon.textContent = '▲';
+          
+        } else {
+          
+          // Si le sous-menu est affiché, le cacher
+          subMenu.style.display = 'none';
+          
+          // Changer l'icône pour indiquer que les sous-liens sont cachés
+          toggleIcon.textContent = '▼';
+        }
+        
+        // Empêcher le comportement par défaut du lien
+        event.preventDefault();
+      });
+    });
   });
   
-  const message = document.querySelector('.search-message');
-  if (filter !== '') { // Vérifie si le champ de recherche n'est pas vide
-      if (found > 0) {
-          message.innerText = `${found} résultats trouvés`; // Utilisation correcte du template string
+
+// Attendez que le DOM soit entièrement chargé avant d'exécuter le script
+document.addEventListener('DOMContentLoaded', function() {
+  
+    // Sélectionnez l'élément bouton de retour en haut de la page
+    const scrollToTopButton = document.querySelector('.scroll-to-top');
+  
+    // Ajoutez un écouteur d'événements pour le défilement de la fenêtre
+    window.addEventListener('scroll', function() {
+      
+      // Vérifiez si le défilement de la fenêtre est supérieur à 300 pixels
+      if (window.pageYOffset > 300) {
+        
+        // Si le défilement est supérieur à 300 pixels, ajoutez une classe CSS pour afficher le bouton
+        scrollToTopButton.classList.add('show');
       } else {
-          message.innerText = 'Aucun résultat trouvé.';
-      }
-  } else {
-      message.innerText = ''; // Cache le message si le champ de recherche est vide
-  }
-});
-// Gestionnaire d'événements pour la touche "Entrée" ou "Aller à"
-searchInput.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter' || event.key === 'Go') {
-    const filter = searchInput.value.trim().toUpperCase();
-    const links = document.querySelectorAll('.sidebar ul li');
-
-    links.forEach(link => {
-      if (link.innerText.toUpperCase().indexOf(filter) > -1) {
-        link.click(); // Simuler un clic sur le lien correspondant
-
-        // Afficher le contenu associé au lien correspondant
-        const targetId = link.getAttribute('data-target'); // Obtient l'ID de la cible du lien
-        const targetContent = document.getElementById(targetId); // Sélectionne l'élément de contenu par son ID
-
-        if (targetContent) {
-          targetContent.style.display = 'block'; // Affiche le contenu associé
-        }
-
-        sidebar.classList.remove('show'); // Ferme le panneau latéral
+        
+        // Sinon, retirez la classe CSS pour cacher le bouton
+        scrollToTopButton.classList.remove('show');
       }
     });
-  }
-});
-
   
-  const clearSearchIcon = document.querySelector('.clear-search-icon');
-clearSearchIcon.addEventListener('click', () => {
-  searchInput.value = ''; // Vide le champ de saisie
-
-  const links = document.querySelectorAll('.sidebar ul li');
-  links.forEach(link => {
-    link.style.display = ''; // Réinitialise l'affichage de tous les liens pour qu'ils soient visibles
-  });
-
-  const message = document.querySelector('.search-message');
-  message.innerText = ''; // Cache le message
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  const parents = document.querySelectorAll('.parent');
-  
-  parents.forEach(parent => {
-      const toggleIcon = parent.querySelector('.toggle-icon');
-      parent.addEventListener('click', function(event) {
-          const subMenu = this.querySelector('.sub-menu');
-          if (subMenu.style.display === 'none' || subMenu.style.display === '') {
-              subMenu.style.display = 'block';
-              toggleIcon.textContent = '▲'; // Icone lorsque les sous-liens sont affichés
-          } else {
-              subMenu.style.display = 'none';
-              toggleIcon.textContent = '▼'; // Icone lorsque les sous-liens sont cachés
-          }
-          event.preventDefault();
-      });
-  });
-});
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  const scrollToTopButton = document.querySelector('.scroll-to-top');
-
-  window.addEventListener('scroll', function() {
-      if (window.pageYOffset > 300) {
-          scrollToTopButton.classList.add('show');
-      } else {
-          scrollToTopButton.classList.remove('show');
-      }
-  });
-
-  scrollToTopButton.addEventListener('click', function() {
+    // Ajoutez un écouteur d'événements pour le clic sur le bouton de retour en haut de la page
+    scrollToTopButton.addEventListener('click', function() {
+      
+      // Faites défiler la fenêtre jusqu'en haut avec une animation en douceur
       window.scrollTo({
-          top: 0,
-          behavior: 'smooth'
+        top: 0,
+        behavior: 'smooth'
       });
+    });
   });
-});
-
+  
+// Attendre que le DOM soit entièrement chargé avant d'exécuter le script
 document.addEventListener("DOMContentLoaded", function() {
-  var liens = document.querySelectorAll(".lien");
-  liens.forEach(function(lien) {
+  
+    // Sélectionner tous les éléments avec la classe "lien"
+    var liens = document.querySelectorAll(".lien");
+    
+    // Pour chaque lien sélectionné
+    liens.forEach(function(lien) {
+      
+      // Ajouter un écouteur d'événements pour le clic sur le lien
       lien.addEventListener("click", function(event) {
-          event.preventDefault(); // Empêcher le comportement par défaut du lien
-          var contenuId = this.getAttribute("data-contenu");
-          var contenu = document.getElementById("contenu_" + contenuId);
-          if (contenu) {
-              // Afficher le contenu associé et masquer les autres contenus
-              var tousLesContenus = document.querySelectorAll(".contenu");
-              tousLesContenus.forEach(function(c) {
-                  c.style.display = "none";
-              });
-              contenu.style.display = "block";
-          }
+        
+        // Empêcher le comportement par défaut du lien
+        event.preventDefault();
+        
+        // Récupérer l'identifiant du contenu associé à partir de l'attribut "data-contenu" du lien
+        var contenuId = this.getAttribute("data-contenu");
+        
+        // Sélectionner le contenu associé à partir de son identifiant
+        var contenu = document.getElementById("contenu_" + contenuId);
+        
+        // Vérifier si le contenu existe
+        if (contenu) {
+          
+          // Afficher le contenu associé et masquer les autres contenus
+          var tousLesContenus = document.querySelectorAll(".contenu");
+          tousLesContenus.forEach(function(c) {
+            c.style.display = "none";
+          });
+          contenu.style.display = "block";
+        }
       });
+    });
   });
-});
+  
+
